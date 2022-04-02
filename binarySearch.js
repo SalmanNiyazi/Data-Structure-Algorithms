89bbf82d1ab8b5418dbc28762af7288ec07d1024
Salman Niyazi <36467313+SalmanNiyazi@users.noreply.github.com>
2022-04-02 16:52:01 +0530

    Update binarySearch.js

--- binarySearch.js
+++ binarySearch.js
@@ -1,4 +1,47 @@
-//program to implement recursive binary search
+//Simple implementation of binary search in C++
+
+#include<iostream>
+using namespace std;
+
+int binarySearch(int arr[], int size, int key){
+    int start = 0;
+    int end  = size-1;
+
+    int mid = (start + (end-start)/2);
+
+    while(start <= end){
+        if(arr[mid] == key){
+            return mid;
+        }
+        if(key >= arr[mid]){
+            start = mid + 1;
+        }
+        else {
+            end = mid - 1;
+        }
+        mid = (start + end)/2;  
+    }
+    return -1;
+}
+
+int main(){
+        int even[6] = {2,4,6,8,12,18};
+        int odd[5] = {3,8,11,14,16};
+
+    int index  = binarySearch(odd, 6, 20);
+    cout<< "Index of 14 is "<<index<<endl;
+
+
+    return 0;
+}
+
+//time complexity of binary search is O(logn)
+
+
+
+
+
+//program to implement recursive binary search in Javascript
 //A recursive binary search function that returns location of x in given array arr[l...r], if present otherwise -1.
 function binarySearch(arr,l,r,x){
     if(r >= 1){
